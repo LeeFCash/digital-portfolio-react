4ab8144cadd184dc7d2bdab05306d760b22fff66
LeeFCash <leecash133@gmail.com>
2024-04-30 19:05:36 -0400

    started page0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <header>
+    <nav><ul>
+      <li><button><a href="./aboutMeTab.html">About me</a></button></li>
+      <li><button><a href="./skills.html">skills</a></button></li>
+      <li><button><a href="./contactMe.html">contact me</a></button></li>
+    </ul></nav>
+  </header>
+  <main>
+    <p class="beforeTabinfo">Hello, my name is Lee Cash, and you can get to know me better by clicking any of the 3 tabs at the top.</p>
+  </main>
     </div>
   );
 }
